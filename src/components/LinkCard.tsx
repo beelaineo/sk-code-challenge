30e4379a2ef6729aa649147644dbf8445334b6d1
Blaine <blaine.d.oneill@gmail.com>
2021-08-08 15:18:27 -0400

    generic LinkCard document

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react'
+import Link from './Link'
+import { Document } from '../types'
 
 interface LinkCardProps {
-  document: 
+  document: Document
 }
 
 export const LinkCard: React.FC<LinkCardProps> = (props) => {
-  return <div>...</div>
+  // switch (props.type) {
+  //   case 'product':
+  //     return ___
+  //   case 'collection':
+  //     return
+  //   case 'page':
+  //     return
+  //   case 'custom':
+  //     return
+  //   default:
+  //     // @ts-ignore
+  //     console.warn(`Link card for type "${props.type}" is missing or nonexistent!`)
+  //     return null
+  // }
+  return (
+  <div className={`card ${props.document._type}`} id={'card'+props.document._id}>
+    <small>{props.document._type}</small>
+    <h2>{props.document.title}</h2>
+    <div>{props.document.summary || props.document.description || null}</div>
+    <Link document={props.document} />
+  </div>
+  )
 }
-
